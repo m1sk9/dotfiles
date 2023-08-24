b4764cb5a9bd14c8fce29b7e488c388a484e0424
m1sk9 <me@m1sk9.dev>
2023-08-25 04:30:26 +0900

    feat: added installer

---
+++ installer.sh
@@ -0,0 +1,24 @@
+read -p "Are you sure? (During execution, you will be asked to enter your password and other information.) [y/N]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+sudo softwareupdate --install-rosetta --agree-to-license
+
+./symlink.sh
+
+brew bundle --file '~/Brewfile'
+
+brew autoupdate start
+
+sudo chsh -s /opt/homebrew/bin/fish
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+fisher install decors/fish-ghq
+
+fisher install jorgebucaran/fish-nvm
+
+fish
