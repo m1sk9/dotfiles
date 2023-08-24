b997f1e7f85be8685e6ac94aab933dca14cb40ac
m1sk9 <me@m1sk9.dev>
2023-08-25 04:20:59 +0900

    shell: added symlink.sh

---
+++ symlink.sh
@@ -0,0 +1,17 @@
+read -p "Are you sure? [y/N]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+echo
+
+ln -sfvn $PWD/config/alacritty $HOME/.config/alacritty
+ln -sfvn $PWD/config/fish $HOME/.config/fish
+ln -sfvn $PWD/config/gh $HOME/.config/gh
+ln -sfvn $PWD/config/lazygit $HOME/.config/lazygit
+ln -sfvn $PWD/config/git/ignore $HOME/.config/ignore
+ln -sfvn $PWD/ssh/smartcard.pub $HOME/.ssh/smartcard.pub
+
+ln -sfv $PWD/config/git/.gitconfig $HOME/.gitconfig
+ln -sfv $PWD/config/Brewfile $HOME/Brewfile
+ln -sfv $PWD/config/tmux/.tmux.conf $HOME/.tmux.conf
