7a9a860bd6ca6633da168db398ea5dfd2f6ba090
m1sk9 <me@m1sk9.dev>
2024-02-10 22:44:37 +0900

    refactor: move config dir

--- symlink.sh
+++ symlink.sh
@@ -1,21 +1,21 @@
 # .config
 mkdir -p "$HOME"/.config/alacritty
-ln -sfvn "$HOME"/dotfiles/config/alacritty/alacritty.toml "$HOME"/.config/alacritty/alacritty.toml
-ln -sfvn "$HOME"/dotfiles/config/alacritty/theme.toml "$HOME"/.config/alacritty/theme.toml
+ln -sfvn "$HOME"/dotfiles/alacritty/alacritty.toml "$HOME"/.config/alacritty/alacritty.toml
+ln -sfvn "$HOME"/dotfiles/alacritty/theme.toml "$HOME"/.config/alacritty/theme.toml
 
 mkdir -p "$HOME"/.config/gh
-ln -sfvn "$HOME"/dotfiles/config/gh/config.yml "$HOME"/.config/gh/config.yml
-ln -sfvn "$HOME"/dotfiles/config/gh/hosts.yml "$HOME"/.config/gh/hosts.yml
+ln -sfvn "$HOME"/dotfiles/gh/config.yml "$HOME"/.config/gh/config.yml
+ln -sfvn "$HOME"/dotfiles/gh/hosts.yml "$HOME"/.config/gh/hosts.yml
 
 mkdir -p "$HOME"/.config/lazygit
-ln -sfvn "$HOME"/dotfiles/config/lazygit/config.yml "$HOME"/.config/lazygit/config.yml
+ln -sfvn "$HOME"/dotfiles/lazygit/config.yml "$HOME"/.config/lazygit/config.yml
 
 mkdir -p "$HOME"/.config/git
-ln -sfvn "$HOME"/dotfiles/config/git/ignore "$HOME"/.config/git/ignore
+ln -sfvn "$HOME"/dotfiles/git/ignore "$HOME"/.config/git/ignore
 
 mkdir -p "$HOME"/.config/fish
-ln -sfvn "$HOME"/dotfiles/config/fish/config.fish "$HOME"/.config/fish/config.fish
+ln -sfvn "$HOME"/dotfiles/fish/config.fish "$HOME"/.config/fish/config.fish
 
 # home
-ln -sfvn "$HOME"/dotfiles/config/git/.gitconfig "$HOME"/.gitconfig
-ln -sfvn "$HOME"/dotfiles/config/Brewfile "$HOME"/Brewfile
+ln -sfvn "$HOME"/dotfiles/git/.gitconfig "$HOME"/.gitconfig
+ln -sfvn "$HOME"/dotfiles/Brewfile "$HOME"/Brewfile
