ef10db35d74ebf0b549c6ab3b721595f2a982e54
m1sk9 <me@m1sk9.dev>
2024-02-10 22:36:03 +0900

    fish: remove unused path and alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,22 +2,21 @@
 alias clone='ghr clone'
 alias lg='lazygit'
 alias ld='lazydocker'
-alias v='code ./'
 alias cat='bat'
 alias find='fd'
-
-alias sbtss='sbt scalafixAll && sbt scalafmtAll'
 
 alias jdk21='set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home"'
 alias jdk17='set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"'
 alias jdk8='set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home"'
 
 # setting path
-set -x SSH_AUTH_SOCK "$(/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket)"
 set -x PATH "$DENO_INSTALL/bin:$PATH"
 set -x PATH "$HOME/.deno/bin:$PATH"
 set -x PATH "$PATH:$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
 set -x PATH "$PATH:/usr/local/bin"
+
+# setting environment variables
+set -x SSH_AUTH_SOCK "$(/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket)"
 set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home"
 set -x DENO_INSTALL "$HOME/.deno"
 set -x XDG_CONFIG_HOME "$HOME/.config"
