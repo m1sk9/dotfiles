80a28d6d9aaaf347d47af2ae285d177367c2fae1
Sho Sakuma <me@m1sk9.dev>
2024-04-18 21:31:52 +0900

    Update config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,5 @@
 # setting alias
 alias clone='ghr clone'
-alias clone-ssh='ghr clone git@github.com:'
 alias lg='lazygit'
 alias ld='lazydocker'
 alias c='code-insiders'
@@ -11,10 +10,6 @@ alias l='eza -abghHliS'
 alias cat='bat'
 alias find='fd'
 
-alias jdk21='set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home"'
-alias jdk17='set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"'
-alias jdk8='set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home"'
-
 # setting path
 set -x PATH "$DENO_INSTALL/bin:$PATH"
 set -x PATH "$HOME/.deno/bin:$PATH"
@@ -23,8 +18,6 @@ set -x PATH "$PATH:/usr/local/bin"
 
 # setting environment variables
 set -x SSH_AUTH_SOCK "$(/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket)"
-## Fucking Gradle
-set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
 set -x DENO_INSTALL "$HOME/.deno"
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x GHR_ROOT "$HOME/projects"
