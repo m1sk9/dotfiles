68399c852a4d5e83e9948b000646c962e2a325e4
m1sk9 <me@m1sk9.dev>
2024-05-30 23:06:03 +0900

    Add .config/nvim/lua/options.lua

---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,13 @@
+local opt = vim.opt
+
+opt.ambiwidth = 'double'
+opt.autoindent = true
+opt.smartindent = true
+opt.cursorline = true
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.relativenumber = true
+opt.termguicolors = true
