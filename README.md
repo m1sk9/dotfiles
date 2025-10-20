71798eccf4eee0d0db7740497735423de9a51a48
Sho Sakuma <me@m1sk9.dev>
2025-10-20 14:52:21 +0900

    update

--- README.md
+++
@@ -1,7 +0,0 @@
-# m1sk9/dotfiles
-
-- Shell: Fish
-- Editor: VSCode and Vim
-- IDE: JetBrains IDE
-- Terminal: Ghostty
-- Web Browser: Firefox Nightly
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,12 +4,12 @@ alias ls='eza'
 alias l='eza -abghHliS'
 alias find='fd'
 alias grep='rg'
+alias zed='zed-preview'
 
 set -x EDITOR "/usr/bin/vim"
 set -x SSH_AUTH_SOCK "$(/opt/homebrew/bin/gpgconf --list-dirs agent-ssh-socket)"
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x GHR_ROOT "$HOME/Projects"
-set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
 
 fish_add_path /opt/homebrew/bin
 fish_add_path /usr/bin
@@ -19,11 +19,9 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.deno/bin
 
 # setting fish
-starship init fish | source
 eval "$(/opt/homebrew/bin/brew shellenv)"
 ghr shell fish | source
 mise activate fish | source
 gpg-connect-agent /bye
 
 export LANG=en_US.UTF-8
-
