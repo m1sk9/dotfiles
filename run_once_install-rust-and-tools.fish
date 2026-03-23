b90637af66003e267cedf0222a0cb1117d7cca75
Sho Sakuma <me@m1sk9.dev>
2026-03-23 22:45:57 +0900

    Add .config/guard-and-guide/rules.toml Add install-rust-and-tools.fish

---
+++ run_once_install-rust-and-tools.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+# Install Rust via rustup
+if not command -q rustc
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    # Add cargo to PATH for the current session
+    set -gx PATH $HOME/.cargo/bin $PATH
+end
+
+# Install guard-and-guide
+cargo install --git https://github.com/kawarimidoll/guard-and-guide --tag 26.3.22
