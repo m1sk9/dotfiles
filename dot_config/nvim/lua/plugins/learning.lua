634d33fd575469432ed6562d767833a4fe9d9789
Sho Sakuma <me@m1sk9.dev>
2026-08-13 20:05:13 +0900

    refactor(nvim): remove hardtime.nvim keystroke discipline plugin
    Its hint notifications for hjkl/arrow-key overuse became noisy in
    daily use now that vim motions are already internalized.

--- dot_config/nvim/lua/plugins/learning.lua
+++ dot_config/nvim/lua/plugins/learning.lua
@@ -21,24 +21,4 @@ return {
       },
     },
   },
-
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {
-      -- Why not 既定の "block": 入門初日から入力を弾かれると作業が止まる．
-      -- まず警告だけ出し，hjkl 連打が減ってきたら "block" に上げる
-      restriction_mode = "hint",
-      -- Why not 既定の矢印キー無効化: 上と同じ理由で最初は許可する．
-      -- 慣れたらこのテーブルごと消せば既定の「矢印キー禁止」に戻る
-      disabled_keys = {
-        ["<Up>"] = false,
-        ["<Down>"] = false,
-        ["<Left>"] = false,
-        ["<Right>"] = false,
-      },
-      disable_mouse = false,
-    },
-  },
 }
